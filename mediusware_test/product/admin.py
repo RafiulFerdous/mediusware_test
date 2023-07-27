37653fc5c6669e051cfdc3dec750c00dd3727f10
unknown <rafiulferdous434@gmail.com>
2023-07-27 13:21:06 +0600

    admin

--- mediusware_test/product/admin.py
+++ mediusware_test/product/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Product
+
 # Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'sku', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('title', 'sku')
